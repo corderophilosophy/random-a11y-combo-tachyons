3e08fe51a716a80e78b5b12bdb14972de9b062d2
Dr. Richard Cordero <rich@corderophilosophy.com>
2016-12-19 08:42:22 -0500

    Rename vars again

--- src/helpers.js
+++ src/helpers.js
@@ -1,12 +1,12 @@
 'use strict';
 const helpers = {
   getRandomProp: function(object) {
-    let vals = Object.keys(object);
+    var vals = Object.keys(object);
     return object[vals[Math.floor(Math.random() * vals.length)]];
   },
 
   getNameFromValue: function(object, value) {
-    let name = Object.keys(object).filter((el) => {
+    var name = Object.keys(object).filter((el) => {
       return object[el] == value;
     })
     return name.toString();
